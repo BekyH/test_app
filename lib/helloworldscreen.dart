e126735b74f35538445455bd4c1957533db4fd90
@bereketheramo25 <bereketheramo25@gmail.com>
2023-09-15 00:06:15 +0300

    added background music

--- lib/helloworldscreen.dart
+++ lib/helloworldscreen.dart
@@ -1,79 +1,73 @@
 import 'dart:math';
 
+import 'package:audio_session/audio_session.dart';
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 
 class Helloworldscreen extends StatefulWidget {
-  const Helloworldscreen({ Key? key }) : super(key: key);
+  const Helloworldscreen({Key? key}) : super(key: key);
 
   @override
   _HelloworldscreenState createState() => _HelloworldscreenState();
 }
 
-class _HelloworldscreenState extends State<Helloworldscreen> with SingleTickerProviderStateMixin {
+class _HelloworldscreenState extends State<Helloworldscreen> {
   Color backgroundColor = Colors.white;
-  late AnimationController animationController;
-  late Animation<Color?> colorTween;
- 
-@override
+  final AudioPlayer _player = AudioPlayer();
+
+  @override
   void initState() {
     super.initState();
-
-    // Initialize the animation controller
-    animationController = AnimationController(
-      duration: Duration(milliseconds: 1000), // Animation duration
-      vsync: this,
-    );
-    // Initialize the color tween
-    colorTween = ColorTween(
-      begin: backgroundColor,
-      end: backgroundColor,
-    ).animate(animationController);
+    _init();
   }
-  void changeBackgroundColor(){
-     const int max = 256;
-     const double opacity =0.1;
-      
-        final random = Random();
-        final newColor = Color.fromRGBO(random.nextInt(max),
-         random.nextInt(max), random.nextInt(max), opacity);
 
-         setState(() {
-            colorTween = ColorTween(
-        begin: backgroundColor,
-        end: newColor,
-      ).animate(animationController);
-      animationController.forward(from: 0.0);
-      backgroundColor = newColor;
+  Future<void> _init() async {
+    final session = await AudioSession.instance;
+    await session.configure(const AudioSessionConfiguration.speech());
+    // Listen to errors during playback.
+    _player.playbackEventStream
+        .listen((event) {}, onError: (Object e, StackTrace stackTrace) {});
+
+    try {
+      await _player
+          .setAudioSource(AudioSource.asset("assets/music/background.mp3"));
+      await _player.play();
+    } catch (e) {
+      print("Error loading audio source: $e");
+    }
+  }
+
+  void changeBackgroundColor() {
+    const int max = 256;
+    const double opacity = 0.1;
+
+    setState(() {
+      final random = Random();
+      backgroundColor = Color.fromRGBO(random.nextInt(max), random.nextInt(max),
+          random.nextInt(max), opacity);
     });
-        
-        
-      
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: changeBackgroundColor,
-      child: AnimatedBuilder(
-          animation: colorTween,
-          builder: (context, child) {
-            return Container(
-              color: colorTween.value, // Set the background color
-              child: Center(
-                child: Text(
-                  'Hello, World!',
-                  style: TextStyle(fontSize: 24),
-                ),
-              ),
-            );
-            },
+      child: AnimatedContainer(
+        duration: Duration(seconds: 1),
+        color: backgroundColor,
+        child: const Center(
+          child: Text(
+            'Hello, World!',
+            style: TextStyle(fontSize: 24,decoration: TextDecoration.none),
+          ),
         ),
-      
+      ),
     );
   }
- @override
+
+  @override
   void dispose() {
-    animationController.dispose();
+    _player.dispose();
     super.dispose();
   }
 }
